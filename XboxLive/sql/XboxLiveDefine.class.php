41d6b6f7217e5c399d0740c785ae2721b9d7ae88
Patrick McKinley <patrick@patrick-mckinley.com>
2011-02-14 20:53:07 +0000

    Fixed an issue with table definitions

--- XboxLive/sql/XboxLiveDefine.class.php
+++ XboxLive/sql/XboxLiveDefine.class.php
@@ -11,39 +11,40 @@ class XboxLiveDefine extends BaseDBDescribe
 	{
 		$table = $this->_addTable('gamertag');
 		$table->addColumn('id', 'int(11) NOT NULL auto_increment');
-		$table->addColumn('game_id', 'int(11) NOT NULL');
-		$table->addColumn('gamertag_id', 'int(11) NOT NULL');
-		$table->addColumn('image', 'varchar(255) NOT NULL');
-		$table->addColumn('name', 'varchar(255) NOT NULL');
-		$table->addColumn('description', 'text NOT NULL');
-		$table->addColumn('score', 'int(11) NOT NULL');
-		$table->addColumn('acquired', 'datetime default NULL');
-		$table->addColumn('slug', 'varchar(255) NOT NULL');
+		$table->addColumn('gamertag', 'varchar(255) NOT NULL');
+		$table->addColumn('score', 'int(11) NOT NULL default '0'');
+		$table->addColumn('last_checked', 'datetime NOT NULL default \'0000-00-00 00:00:00\'');
+		$table->addColumn('account_type', 'enum(\'GOLD\', \'SILVER\') NOT NULL default \'SILVER\'')
+		$table->addColumn('link', 'varchar(255) default NULL');
+		$table->addColumn('avatar', 'varchar(255) default NULL');
+		$table->addColumn('zone', 'varchar(255) default NULL');
+		$table->addColumn('reputation_stars', 'varchar(255) default NULL');
 		$table->addColumn('login_data', 'blob');
 		
-		$table = $this->_addTable('game');
-		$table->addColumn('id', 'int(11) NOT NULL auto_increment');
-		$table->addColumn('name', 'varchar(255) NOT NULL');
-		$table->addColumn('total_achievements', 'int(11) NOT NULL');
-		$table->addColumn('total_score', 'int(11) NOT NULL');
-		$table->addColumn('image_64', 'varchar(255) default NULL');
-		$table->addColumn('gamertag_id', 'int(11) NOT NULL');
-		$table->addColumn('last_played', 'date default NULL');
-		$table->addColumn('achievements', 'int(11) NOT NULL default \'0\'');
-		$table->addColumn('score', 'int(11) NOT NULL default \'0\'');
-		$table->addColumn('slug', 'varchar(255) NOT NULL');
-		$table->addColumn('link', 'varchar(255) NOT NULL');
+		$gameTable = $this->_addTable('game');
+		$gameTable->addColumn('id', 'int(11) NOT NULL auto_increment');
+		$gameTable->addColumn('name', 'varchar(255) NOT NULL');
+		$gameTable->addColumn('total_achievements', 'int(11) NOT NULL');
+		$gameTable->addColumn('total_score', 'int(11) NOT NULL');
+		$gameTable->addColumn('image_64', 'varchar(255) default NULL');
+		$gameTable->addColumn('gamertag_id', 'int(11) NOT NULL');
+		$gameTable->addColumn('last_played', 'date default NULL');
+		$gameTable->addColumn('achievements', 'int(11) NOT NULL default \'0\'');
+		$gameTable->addColumn('score', 'int(11) NOT NULL default \'0\'');
+		$gameTable->addColumn('slug', 'varchar(255) NOT NULL');
+		$gameTable->addColumn('link', 'varchar(255) NOT NULL');
 
-		$table = $this->_addTable('achievement');
-		$table->addColumn('id', 'int(11) NOT NULL auto_increment');
-		$table->addColumn('game_id', 'int(11) NOT NULL');
-		$table->addColumn('gamertag_id', 'int(11) NOT NULL');
-		$table->addColumn('image', 'varchar(255) NOT NULL');
-		$table->addColumn('name', 'varchar(255) NOT NULL');
-		$table->addColumn('description', 'text NOT NULL');
-		$table->addColumn('score', 'int(11) NOT NULL');
-		$table->addColumn('acquired', 'datetime default NULL');
-		$table->addColumn('slug', 'varchar(255) NOT NULL');
+		
+		$achievementTable = $this->_addTable('achievement');
+		$achievementTable->addColumn('id', 'int(11) NOT NULL auto_increment');
+		$achievementTable->addColumn('game_id', 'int(11) NOT NULL');
+		$achievementTable->addColumn('gamertag_id', 'int(11) NOT NULL');
+		$achievementTable->addColumn('image', 'varchar(255) NOT NULL');
+		$achievementTable->addColumn('name', 'varchar(255) NOT NULL');
+		$achievementTable->addColumn('description', 'text NOT NULL');
+		$achievementTable->addColumn('score', 'int(11) NOT NULL');
+		$achievementTable->addColumn('acquired', 'datetime default NULL');
+		$achievementTable->addColumn('slug', 'varchar(255) NOT NULL');
 		
 		return $this;
 	}
