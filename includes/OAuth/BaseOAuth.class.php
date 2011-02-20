a843d97449e7eecc99b8300af2b3f3c24780a7c1
Patrick McKinley <patrick@patrick-mckinley.com>
2011-02-20 01:49:55 +0000

    Changing access rights on the internal constructor on BaseOAuth class

--- includes/OAuth/BaseOAuth.class.php
+++ includes/OAuth/BaseOAuth.class.php
@@ -44,7 +44,7 @@ abstract class BaseOAuth extends BaseObject
 	 * 
 	 * @return void
 	 */
-	public function _construct()
+	protected function _construct()
 	{
 		$this->resetParams();
 		return parent::_construct();
